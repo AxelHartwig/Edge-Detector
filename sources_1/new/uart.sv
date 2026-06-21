73abcc9139e2b9b976788c6d664009edaae0d839
AxelHartwig <axel@hartwig.nu>
2026-06-21 22:37:52 +0200

    first commit

---
+++ sources_1/new/uart.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/21/2026 10:35:45 PM
+// Design Name: 
+// Module Name: uart
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module uart(
+
+    );
+endmodule
